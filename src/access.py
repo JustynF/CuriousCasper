918b1e203e9c4366393ab159382643109bd0c49f
JustynF <florendojustyn@gmail.com>
2019-02-20 18:20:15 -0500

    implemented inverted index

--- src/access.py
+++ src/access.py
@@ -1,23 +1,12 @@
 import json
 from csipreprocess import csiPreprocess
-from vectorspacemodel import VectorSpaceModel
-from dictionarybuilding import DictionaryBuilding
 
 class Access:
     def __init__(self):
-        with open("corpus.json", "r") as corpus_file:
+        with open("./src/corpus.json", "r") as corpus_file:
             self.corpus = json.load(corpus_file)
 
     def corpus_access(self, docid):
         for doc in self.corpus:
-            if doc['docID'] in docid:
-                return doc
-
-def main():
-    x = {}
-    test = VectorSpaceModel(x)
-    test.calculate_idf()
-
-
-if __name__ == "__main__":
-    main()
+            if doc['docId'] == docid:
+                return doc['title']
