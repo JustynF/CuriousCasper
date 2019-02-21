6b6d119264e5b40625e75f1a323947bb49424a1e
mcolo031 <32468411+mcolo031@users.noreply.github.com>
2019-02-21 16:08:13 -0500

    VectorSpace updated

--- src/vectorspacemodel.py
+++ src/vectorspacemodel.py
@@ -1,7 +1,10 @@
+import collections
 import json
 import math
 from collections import Counter, defaultdict
+
 import re
+
 
 class VectorSpaceModel:
 
@@ -10,26 +13,74 @@ class VectorSpaceModel:
         with open("corpus.json", "r") as corpus_file:
             self.corpus = json.load(corpus_file)
 
-        self.mode = 'original'
-        self.idf_dict = defaultdict(list)
+        self.idf_dict = {}
         self.tf_idf_dict = defaultdict(lambda: defaultdict(int))
 
     def words(self, s):
-        return re.findall(r"\w+", s, re.UNICODE | re.IGNORECASE)
+        ''' Ignore, used for testing'''
+        number_of_occurences = 0
+        for word in s.split():
+            if word == 'a':
+                number_of_occurences += 1
 
     def calculate_idf(self, inverted_index):
+        ''' Calculates the idf of all words and returns a list'''
         N = len(self.corpus)
-        for word, docs in inverted_index.items():
-            self.idf_dict[word].append(math.log10(N/len(docs)))
+        for word, docs in inverted_index.iteritems():
+            self.idf_dict[word] = math.log10(N/len(docs))
+        print(len(self.idf_dict))
         return self.idf_dict.items()
 
-    def calculate_tf_idf(self, inverted_index):
-        for word, docs in inverted_index.items():
-            temp = defaultdict(int)
-            for doc in self.corpus:
-                temp[doc] = 0
-            for occurances in docs:
-                temp[occurances.doc_id] = occurances.frequency * self.idf_dict[word]
-            self.tf_idf_dict[word] = temp
-        return self.tf_idf_dict.items()
+    def count(self, word, docId):
+        ''' Counts number of occurances for a given word in a given document'''
+        cnt = 0
+        strnew = self.get_document(docId)
+        strnew = strnew['text']
+        for elem in strnew:
+            if elem == word:
+                cnt += 1
+        return cnt
+
+    def get_document(self, docId):
+        ''' Returns a document given docId'''
+        for doc in self.corpus:
+            if doc['docId'] == docId:
+                return doc
+
+
+    def calculate_tf_idf(self, inverted_index, idf_index):
+        ''' Calculates tfidf'''
+        tf_idf = defaultdict(lambda: defaultdict(int))
+        for words, docs in inverted_index.iteritems():
+            placeholder = defaultdict(int)
+            for occurance in docs:
+                placeholder[words] = self.count(words, occurance) * self.idf_dict[words]
+            tf_idf[words] = placeholder
+
+        return tf_idf
+
+    def calculate_doc_vecs(self, tf_idf_matrix, tokens):
+        ''' Calculates doc vectors per document given a set of tokens'''
+        doc_vecs = defaultdict(tuple)
+        for doc_id in self.corpus:
+            vector = []
+            for token in tokens:
+                doc_weights = tf_idf_matrix[token]
+                weight = tf_idf_matrix[doc_id['docId']]
+                vector.append(weight)
+            doc_vecs[doc_id['docId']] = vector
+        return doc_vecs
+
+
+    def calculate_vec_score(self,query_vector, doc_vecs):
+        ''' Sets up scores for each document dependant on the query given'''
+        scores = []
+        for doc_id, vector in doc_vecs:
+            score = 0
+            for terms, doc_tf_idf in query_vector:
+                score += terms * doc_tf_idf
+            scores.append((doc_id, score))
+        return scores
+
+
 
